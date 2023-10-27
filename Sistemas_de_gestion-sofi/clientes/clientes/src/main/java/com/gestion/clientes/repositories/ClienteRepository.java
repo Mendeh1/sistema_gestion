10042eb1629ad12ad52be4515ea80dcb22195260
Mendeh1 <sebasandres12342@gmail.com>
2023-10-27 10:34:48 -0500

    1

---
+++ Sistemas_de_gestion-sofi/clientes/clientes/src/main/java/com/gestion/clientes/repositories/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.gestion.clientes.repositories;
+import com.gestion.clientes.dto.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
